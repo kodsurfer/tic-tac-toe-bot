6e013bcc50160aad79f2fa7e963c4a97ca669521
kodsurfer <kodsurfer@pm.me>
2025-03-19 15:35:11 +0300

    wip

--- main.rb
+++ main.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/ruby
 # frozen_string_literal: true
+require 'telegram/bot'
+require 'dotenv/load'
+
+token = env['tg_bot_token']
+
+class TicTacToe
+    attr_accessor :board, :cur_player
+    
+    def initialize
+        @board = Array.new(3) { Array.new(3, ' ')}        
+        @cur_player = 'X'
+    end
+
+    def move(r,c)
+    end
+
+    
+end
